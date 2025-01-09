f4ad98e5d1a8a5617f037a3bcfa39d3b8566bfed
bekione <kinber024@gmail.com>
2025-01-09 06:06:16 +0300

    feat: create Providers component for theme management with next-themes

---
+++ components/providers.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes"
+
+export  function Providers({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
+}
