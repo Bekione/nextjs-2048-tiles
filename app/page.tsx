bcb734872f3b76e02de179c6b9169c10313b1bd3
bekione <kinber024@gmail.com>
2025-01-09 06:07:22 +0300

    feat: add GameBoard component to Home page for game functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import GameBoard from "@/components/game-board";
 
 export default function Home() {
   return (
     <div>
       <h1>2048</h1>
+      <GameBoard />
     </div>
   );
 }
