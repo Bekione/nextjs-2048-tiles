35aad3877d705ff5da0764d978bcfe63f73e2b78
bekione <kinber024@gmail.com>
2025-01-11 02:38:36 +0300

    feat: add GitHub link component to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/header";
 import GameBoard from "@/components/game-board";
 import InstractionCard from "@/components/instraction-card";
-
+import { GitHubLink } from "@/components/github-link";
 export default function Home() {
   return (
-    <div className="h-screen relative flex flex-col items-center">
+    <div className="h-screen relative flex flex-col items-center bg-muted">
       <Header />
       <GameBoard />
       <InstractionCard />
+      <GitHubLink />
     </div>
   );
 }
