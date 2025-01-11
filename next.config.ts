baa81c87d630db6df871c965432b87689f8a5d20
bekione <kinber024@gmail.com>
2025-01-11 18:58:10 +0300

    feat: integrate next-pwa for enhanced PWA configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import withPWA from '@ducanh2912/next-pwa'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const config: NextConfig = {
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+})(config)
+
+export default nextConfig
+
