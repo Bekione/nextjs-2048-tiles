bb4948647fe4a64a0b80394b74c1a27937e1a292
bekione <kinber024@gmail.com>
2025-01-11 20:41:18 +0300

    feat: enhance manifest with additional screenshots and size attributes

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,14 +10,31 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: "/android-chrome-192x192.png",
         type: "image/png",
+        sizes: "192x192",
       },
       {
         src: "/android-chrome-512x512.png",
         type: "image/png",
+        sizes: "512x512",
       },
     ],
     theme_color: "#41a1a4",
     background_color: "#262626",
     display: "standalone",
+    start_url: "/",
+    screenshots: [
+      {
+        src: "/screenshot1.png",
+        sizes: "1280x626",
+        type: "image/png",
+        form_factor: "wide",
+      },
+      {
+        src: "/screenshot2.png",
+        sizes: "430x547",
+        type: "image/png",
+        form_factor: "narrow",
+      },
+    ],
   };
 }
