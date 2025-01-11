77bc98c1bce3a4ddcbdbc9f9a342518640ab6a24
bekione <kinber024@gmail.com>
2025-01-11 15:45:52 +0300

    feat: register service worker in Home component for enhanced offline capabilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+"use client"
+
+import { useEffect } from "react";
 import Header from "@/components/header";
 import GameBoard from "@/components/game-board";
 import InstractionCard from "@/components/instruction-card";
 import { GitHubLink } from "@/components/github-link";
+
 export default function Home() {
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(
+          (registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+          },
+          (err) => {
+            console.log('Service Worker registration failed:', err);
+          }
+        );
+      });
+    }
+  }, []);
+  
   return (
     <div className="h-screen relative flex flex-col items-center bg-muted">
       <Header />
