0031bb9fffdc09145a3fa2894808d6d37bb42333
bekione <kinber024@gmail.com>
2025-01-11 04:50:47 +0300

    feat: enhance metadata for improved SEO and social sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,29 @@ export const metadata: Metadata = {
   title: "2048 with Next.js 15",
   description:
     "The famous 2048 game built with Next.js 15 and modern components.",
+  keywords: "2048, Next.js, game, modern components",
+  authors: [{ name: "Bereket Kinfe" }],
+  openGraph: {
+    title: "2048 with Next.js 15",
+    description: "The famous 2048 game built with Next.js 15 and modern components.",
+    url: "https://nextjs-2048-tiles.vercel.app",
+    type: "website",
+    images: [
+      {
+        url: "https://nextjs-2048-tiles.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "2048 Game",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@bekione23",
+    title: "2048 with Next.js 15",
+    description: "The famous 2048 game built with Next.js 15 and modern components.",
+    images: "https://nextjs-2048-tiles.vercel.app/og-image.jpg",
+  },
 };
 
 export default function RootLayout({
@@ -30,9 +53,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Providers>{children}</Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
-        
 }
