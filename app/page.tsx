7731724fd26c56cc29b7880e34be33f444c6eea0
bekione <kinber024@gmail.com>
2025-01-12 15:56:46 +0300

    feat: integrate Cloudinary image loader and send base URL to service worker

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,46 @@ import Header from "@/components/header";
 import GameBoard from "@/components/game-board";
 import InstructionCard from "@/components/instruction-card";
 import { GitHubLink } from "@/components/github-link";
+import Image from "next/image";
+import cloudinaryLoader from "@/lib/image-loader";
+import { gifNumbers } from "@/components/tile";
 
 export default function Home() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
+      const cloudinaryBaseUrl = process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL;
+
       window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/service-worker.js").then(
-          (registration) => {
-            console.log(
-              "Service Worker registered with scope:",
-              registration.scope
-            );
-          },
-          (error) => {
+        navigator.serviceWorker
+          .register("/service-worker.js")
+          .then((registration) => {
+            console.log("Service Worker registered with scope:", registration.scope);
+
+            // Send the Cloudinary base URL to the service worker
+            registration.active?.postMessage({ cloudinaryBaseUrl });
+          })
+          .catch((error) => {
             console.error("Service Worker registration failed:", error);
-          }
-        );
+          });
       });
     }
   }, []);
 
   return (
     <div className="h-screen relative flex flex-col items-center bg-muted">
+      <div style={{ display: "none" }}>
+          {gifNumbers.map((src) => (
+            <Image
+              loader={cloudinaryLoader}
+              key={src}
+              src={`/gifs/${src}.webp`}
+              alt=""
+              width={128}
+              height={128}
+              priority
+            />
+          ))}
+        </div>
       <Header />
       <GameBoard />
       <InstructionCard />
